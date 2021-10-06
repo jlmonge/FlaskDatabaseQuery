b1a778eca8c630b61ddc98ed8551ff02d926ce4d
firefirehot <rwyat002@ucr.edu>
2021-10-06 13:48:30 -0700

    Basic client-server with annotations

---
+++ app_interface.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request#imports from flask
+from userInput import exampleForm#import forms here. We import these to keep ourselves organized.
+#notice here that homepage.html does not need to be passed in. That is because it is in the templates folder
+#In the future we might use templates to reduce redundant html code.
+
+app = Flask(__name__)#neccessary for flask
+
+@app.route("/", methods=['POST','GET'])#creates "/" directory and accepts 'POST' and 'GET' Requests
+def hello_world():
+    if request.method == 'POST': #will only run below code if client is posting
+        #below code: exampleForm is just an imported class.
+        #request.form looks at the html in the render_template function.
+        # It finds the input given the name 'nm' and returns the user input.
+        form = exampleForm(request.form["nm"])
+        print(form.nm) #prints recieved user input on terminal
+    return render_template('homePage.html')
+
---
+++ userInput.py
@@ -0,0 +1,10 @@
+# This is the file to make forms. Forms organized collections of user data.
+# a good example of a form is registration. A registration form will accept 
+#   and check a username and password.
+
+#The information collected on a form will be added to a database
+
+
+class exampleForm:#very basic form. It does not error check.
+    def __init__(self, passedName):
+        self.nm = passedName
