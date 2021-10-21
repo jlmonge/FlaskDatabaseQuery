39c15390668421dd2debd4a0c8cf417c538d7e93
firefirehot <rwyat002@ucr.edu>
2021-10-20 17:26:53 -0700

    push for merging

--- app_interface.py
+++ app_interface.py
@@ -8,8 +8,11 @@ from add_function import add_to_json
 # notice here that index.html does not need to be passed in. That is because it is in the templates folder
 # In the future we might use templates to reduce redundant html code.
 
+# py -m venv env
+# env/Scripts/Activate
 # export FLASK_APP=app_interface.py
 # export FLASK_DEBUG=1
+# flask run
 
 app = Flask(__name__) # neccessary for flask
 databaseFile = os.path.join(app.static_folder, 'WORKING-2018ksprojects.json') # Location of our file. Call it in functions rather than writing this whole thing out again
@@ -108,130 +111,166 @@ def add_kickstarter():
     #else:
     #    return render_template('sentanceMessage.html',message = "error")
 
-@app.route("/id", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
-def search_id():
-    if request.method == 'POST': # will only run below code if client is posting
+def search_helper(key, method="GET"):
+    if method == 'POST': # will only run below code if client is posting
         # below code: exampleForm is just an imported class.
         # request.form looks at the html in the render_template function.
         # It finds the input given the name 'nm' and returns the user input.
-        #form = exampleForm(request.form["id"])
-        id = request.form.get('id')
-        if not id or id.isspace():
+        # form = exampleForm(request.form["id"])
+        value = request.form.get(key)
+        if not value or value.isspace():
             return redirect(request.url)
         #with open('test.txt', 'w') as f:   # needed once we edit values in the json file
             #f.write(id)
-        return redirect(url_for('get_id', id=id))
-    return render_template('search-id.html')
+        return redirect(url_for('results', key=key, value=value))
+    return render_template(f'search-{key.lower()}.html')
 
-@app.route("/id/<id>")
-def get_id(id):
-    project = {} # the project being looked for
-    with open(databaseFile, encoding='utf-8-sig') as json_file:
-        data = json.load(json_file) # json --> dictionary
-        for proj in data:
-            if proj['ID'] == id:
-                project = proj
-    return render_template('get-id.html', project=project)
+app = Flask(__name__) # neccessary for flask
+
+@app.route("/") # creates "/" directory for homepage
+def index():
+    return render_template('index.html')
+
+@app.route("/id", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
+def search_ID():
+    return search_helper('ID', request.method)
 
 @app.route("/name", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
 def search_name():
-    if request.method == 'POST': # will only run below code if client is posting
-        # below code: exampleForm is just an imported class.
-        # request.form looks at the html in the render_template function.
-        # It finds the input given the name 'nm' and returns the user input.
-        #form = exampleForm(request.form["nm"])
-        name = request.form.get('name')
-        if not name or name.isspace():
-            return redirect(request.url)
-        #with open('test.txt', 'w') as f:   # needed once we edit values in the json file
-            #f.write(name)
-        return redirect(url_for('get_name', name=name))
-    return render_template('search-name.html')
-
-@app.route("/name/<name>")
-def get_name(name):
-    projects = [] # the project being looked for
-    with open(databaseFile, encoding='utf-8-sig') as json_file:
-        data = json.load(json_file) # json --> dictionary
-        for proj in data:
-            if name.lower() in proj['name'].lower():
-                projects.append(proj)
-    return render_template('get-name.html', projects=projects)
+    return search_helper('name', request.method)
 
 @app.route("/category", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
 def search_category():
-    if request.method == 'POST': # will only run below code if client is posting
-        # below code: exampleForm is just an imported class.
-        # request.form looks at the html in the render_template function.
-        # It finds the input given the name 'nm' and returns the user input.
-        #form = exampleForm(request.form["id"])
-        category = request.form.get('category')
-        if not category or category.isspace():
-            return redirect(request.url)
-        #with open('test.txt', 'w') as f:   # needed once we edit values in the json file
-            #f.write(id)
-        return redirect(url_for('get_category', category=category))
-    return render_template('search-category.html')
-
-@app.route("/category/<category>")
-def get_category(category):
-    project = {} # the project being looked for
-    with open(databaseFile, encoding='utf-8-sig') as json_file:
-        data = json.load(json_file) # json --> dictionary
-        projects = catagory_search(category)
-
-    return render_template('get-name.html', projects=projects)
-
+    return search_helper('category', request.method)
 
 
 @app.route("/state", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
 def search_state():
-    if request.method == 'POST': # will only run below code if client is posting
-        # below code: exampleForm is just an imported class.
-        # request.form looks at the html in the render_template function.
-        # It finds the input given the name 'nm' and returns the user input.
-        #form = exampleForm(request.form["id"])
-        state = request.form.get('state')
-        if not state or state.isspace():
-            return redirect(request.url)
-        #with open('test.txt', 'w') as f:   # needed once we edit values in the json file
-            #f.write(id)
-        return redirect(url_for('get_state', state=state))
-    return render_template('search-state.html')
+    return search_helper('state', request.method)
 
-@app.route("/state/<state>")
-def get_state(state):
-    project = {} # the project being looked for
-    with open(databaseFile, encoding='utf-8-sig') as json_file:
-        data = json.load(json_file) # json --> dictionary
-        projects = state_search(state)
-
-    return render_template('get-name.html', projects=projects)
-
-
-@app.route("/month", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
+@app.route("/launched", methods=['POST','GET']) # creates "/" directory and accepts 'POST' and 'GET' Requests
 def search_month():
-    if request.method == 'POST': # will only run below code if client is posting
-        # below code: exampleForm is just an imported class.
-        # request.form looks at the html in the render_template function.
-        # It finds the input given the name 'nm' and returns the user input.
-        #form = exampleForm(request.form["id"])
-        month = request.form.get('month')
-        if not month or month.isspace():
-            return redirect(request.url)
-        #with open('test.txt', 'w') as f:   # needed once we edit values in the json file
-            #f.write(id)
-        return redirect(url_for('get_month', month=month))
-    return render_template('search-month.html')
+    return search_helper('launched', request.method)
 
-@app.route("/month/<month>")
-def get_month(month):
-    project = {} # the project being looked for
-    with open(databaseFile, encoding='utf-8-sig') as json_file:
+@app.route("/search/key=<key>&value=<value>")
+def results(key, value):
+    file = os.path.join(app.static_folder, 'ks-projects-201801.json') # location of json file
+    projects = [] # the project(s) being looked for
+    with open(file, encoding='utf-8-sig') as json_file:
         data = json.load(json_file) # json --> dictionary
-        projects = launched_month_search(month)
+        for proj in data:
+            if key == 'ID' and value == proj.get(key):
+                projects.append(proj)
+            elif (key == 'name' or key == 'state' or key == 'category' or key == 'launched') and value.lower() in proj.get(key).lower():
+                projects.append(proj)
+    return render_template('results.html', projects=projects)
 
-    return render_template('get-name.html', projects=projects)
+@app.route("/edit", methods=['POST','GET'])
+def edit_project():
+    if request.method == 'POST':
+        id = request.form.get('id_to_edit')
+        # these if statements prevent flask errors when any new value is left blank
+        if not id or id.isspace():
+            return redirect(request.url)
+        new_id = request.form.get('new_id')
+        if not new_id:
+            new_id = '\n'
+        new_name = request.form.get('new_name')
+        if not new_name:
+            new_name = '\n'
+        new_category = request.form.get('new_category')
+        if not new_category:
+            new_category = '\n'
+        new_main_category = request.form.get('new_main_category')
+        if not new_main_category:
+            new_main_category = '\n'
+        new_currency = request.form.get('new_currency')
+        if not new_currency:
+            new_currency = '\n'
+        new_deadline = request.form.get('new_deadline')
+        if not new_deadline:
+            new_deadline = '\n'
+        new_goal = request.form.get('new_goal')
+        if not new_goal:
+            new_goal = '\n'
+        new_launched = request.form.get('new_launched')
+        if not new_launched:
+            new_launched = '\n'
+        new_pledged = request.form.get('new_pledged')
+        if not new_pledged:
+            new_pledged = '\n'
+        new_state = request.form.get('new_state')
+        if not new_state:
+            new_state = '\n'
+        new_backers = request.form.get('new_backers')
+        if not new_backers:
+            new_backers = '\n'
+        new_country = request.form.get('new_country')
+        if not new_country:
+            new_country = '\n'
+        return redirect(url_for('do_edit', id=id, new_id=new_id, new_name=new_name, new_category=new_category,
+            new_main_category=new_main_category, new_currency=new_currency, new_deadline=new_deadline,
+            new_goal=new_goal, new_launched=new_launched, new_pledged=new_pledged, new_state=new_state,
+            new_backers=new_backers, new_country=new_country))
+    return render_template('edit.html')
+
+@app.route('''/edit/id=<id>&new_id=<new_id>&new_name=<new_name>&new_category=<new_category>
+    &new_main_category=<new_main_category>&new_currency=<new_currency>&new_deadline=<new_deadline>
+    &new_goal=<new_goal>&new_launched=<new_launched>&new_pledged=<new_pledged>&new_state=<new_state>
+    &new_backers=<new_backers>&new_country=<new_country>''', methods=['POST','GET'])
+def do_edit(id, new_id, new_name, new_category, new_main_category, new_currency, new_deadline, new_goal, 
+    new_launched, new_pledged, new_state, new_backers, new_country):
+    file = os.path.join(app.static_folder, 'ks-projects-201801.json') # location of json file
+    projectFound = False # the project being looked for
+    pos = 0
+    with open(file, 'r', encoding='utf-8-sig') as json_file:
+        data = json.load(json_file) # json --> dictionary
+        for proj in data:
+            if id == proj.get('ID'):
+                projectFound = True
+                # these if statements prevent flask errors when any new value is left blank
+                if new_id != '\n':
+                    data[pos]['ID'] = new_id 
+                if new_name != '\n':
+                    data[pos]['name'] = new_name
+                if new_category != '\n':
+                    data[pos]['category'] = new_category
+                if new_main_category != '\n':
+                    data[pos]['main_category'] = new_main_category
+                if new_currency != '\n':
+                    data[pos]['currency'] = new_currency
+                if new_deadline != '\n':
+                    data[pos]['deadline'] = new_deadline
+                if new_goal != '\n':
+                    data[pos]['goal'] = new_goal
+                if new_launched != '\n':
+                    data[pos]['launched'] = new_launched
+                if new_pledged != '\n':
+                    data[pos]['pledged'] = new_pledged
+                if new_state != '\n':
+                    data[pos]['state'] = new_state
+                if new_backers != '\n':
+                    data[pos]['backers'] = new_backers
+                if new_country != '\n':
+                    data[pos]['country'] = new_country
+                break
+            else:
+                pos += 1
+        if projectFound:
+            print("got here")
+        else:
+            return render_template('edit-failure.html')
+    
+    with open(file, 'w', encoding='utf-8-sig') as json_file:
+        json.dump(data, json_file, indent=4)
+    '''
+    print(proj)
+    print(new_id, new_name, new_category, new_main_category, new_currency, new_deadline, new_goal, new_launched, \
+        new_pledged, new_state, new_backers, new_country)
+    print(proj['ID'], proj['name'], proj['category'], proj['main_category'], proj['currency'], proj['deadline'], \
+        proj['goal'], proj['launched'], proj['pledged'], proj['state'], proj['backers'], proj['country'])
+    '''
+    return render_template('edit-success.html', project=proj)
 
 
 
